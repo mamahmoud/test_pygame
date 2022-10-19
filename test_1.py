4c264046ff27cea937adb87dd0be361c7862a1ea
Mohamed Ali <mohamed.ahmed1024@gmail.com>
2022-10-19 19:14:22 +0200

    making enemy move

--- test_1.py
+++ test_1.py
@@ -1,11 +1,17 @@
 import pygame
 import sys
+import random
 
 WIDTH = 800
 HEIGHT = 600
 
 
 def draw_player(screen, img, x_axis, y_axis):
+    # print(x_axis, y_axis)
+    screen.blit(img, (x_axis, y_axis))
+
+
+def draw_enemy(screen, img, x_axis, y_axis):
     # print(x_axis, y_axis)
     screen.blit(img, (x_axis, y_axis))
 
@@ -22,11 +28,17 @@ if __name__ == "__main__":
     # adding image icon
     icon = pygame.image.load("startup.png")
     player_image = pygame.image.load("space-invaders.png")
+    enemy_image = pygame.image.load("alien.png")
     player_x = 370
     player_y = 500
-
+    enemy_x = 380
+    enemy_y = 20
+    enemy_speed = 2
+    player_speed = 10
+    postive_distances = list(range(40, 101, enemy_speed * 5))
+    negative_distances = list(range(-90, -29, enemy_speed * 5))
+    distance = random.choice(postive_distances + negative_distances)
     # game loop
-    i = 0
     while True:
         # change color
         screen1.fill((255, 0, 0))
@@ -37,22 +49,22 @@ if __name__ == "__main__":
                 # up
                 if event.key == pygame.K_UP:
                     if player_y >= 30:
-                        player_y -= 10
+                        player_y -= player_speed
                     break
                 # down
                 elif event.key == pygame.K_DOWN:
                     if player_y <= 520:
-                        player_y += 10
+                        player_y += player_speed
                     break
                 # right
                 elif event.key == pygame.K_RIGHT:
                     if player_x <= 720:
-                        player_x += 10
+                        player_x += player_speed
                     break
                 # left
                 elif event.key == pygame.K_LEFT:
                     if player_x >= 20:
-                        player_x -= 10
+                        player_x -= player_speed
                     break
             if event.type == pygame.KEYUP:
                 if event.key in [
@@ -63,8 +75,31 @@ if __name__ == "__main__":
                 ]:
                     pass
 
-        # draw player
+        # draw aplayer
         draw_player(screen1, player_image, player_x, player_y)
+        # draw enemy
+        if distance == 0:
+            distance = random.choice(negative_distances + postive_distances)
+        if enemy_x >= 725:
+            distance = random.choice(negative_distances)
+            distance += enemy_speed
+            enemy_x -= enemy_speed
+            draw_enemy(screen1, enemy_image, enemy_x, enemy_y)
+        elif enemy_x <= 15:
+            distance = random.choice(postive_distances)
+            distance -= enemy_speed
+            enemy_x += enemy_speed
+            draw_enemy(screen1, enemy_image, enemy_x, enemy_y)
+        elif distance > 0 and enemy_x < 725:
+            distance -= enemy_speed
+            enemy_x += enemy_speed
+            draw_enemy(screen1, enemy_image, enemy_x, enemy_y)
+        elif distance < 0 and enemy_x > 15:
+            distance += enemy_speed
+            enemy_x -= enemy_speed
+            draw_enemy(screen1, enemy_image, enemy_x, enemy_y)
+
+        # draw_enemy(screen1, enemy_image, enemy_x, enemy_y)
         # update animation
         pygame.display.update()
-        clock.tick(240)
+        clock.tick(60)
