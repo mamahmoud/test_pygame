7df9df697e3d7f9b92f89b253f37ebdda2fb2a3f
Mohamed Ali <mohamed.ahmed1024@gmail.com>
2022-10-19 13:09:32 +0200

    adding title

--- test_1.py
+++ test_1.py
@@ -3,8 +3,16 @@ import pygame
 import sys
 
 if __name__ == "__main__":
+    # initialize
     pygame.init()
+    # create window
     screen1 = pygame.display.set_mode((500, 500))
+    # title
+    pygame.display.set_caption("Hello pygame by mohamed")
+    # adding image icon
+    # pygame.image()
+
+    # game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
