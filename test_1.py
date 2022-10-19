999d971e4932192006e4e844eafadd9c9ee63450
Mohamed Ali <mohamed.ahmed1024@gmail.com>
2022-10-19 13:03:31 +0200

    adding quit

--- test_1.py
+++ test_1.py
@@ -1,10 +1,11 @@
+from pickle import FALSE
 import pygame
-
+import sys
 
 if __name__ == "__main__":
     pygame.init()
-
     screen1 = pygame.display.set_mode((500, 500))
-
     while True:
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit(0)
