7f642f83ed3811c4b88769f697b3f3b09177c067
Mohamed Ali <mohamed.ahmed1024@gmail.com>
2022-10-19 16:08:17 +0200

    adding moving with keyboard

--- test_1.py
+++ test_1.py
@@ -21,16 +21,36 @@ if __name__ == "__main__":
     # adding image icon
     icon = pygame.image.load("startup.png")
     img = pygame.image.load("startup_2.png")
+    player_x = 370
+    player_y = 500
 
     # game loop
+    i = 0
     while True:
         # change color
         screen1.fill((255, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit(0)
+            if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
+                print(event.key)
+                if event.key == 1073741906:
+                    player_y -= 10
+                    break
+                elif event.key == 1073741905:
+                    player_y += 10
+                    break
+                elif event.key == 1073741903:
+                    player_x += 10
+                    break
+                elif event.key == 1073741904:
+                    player_x -= 10
+                    break
+
         # draw player
-        draw_player(screen1, img, 370, 500)
+        draw_player(screen1, img, player_x, player_y)
+        # draw_player(screen1, img, i, i)
+        i += 1
         # update animation
         pygame.display.update()
         clock.tick(240)
